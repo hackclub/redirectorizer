0da24390a8aeef3010dd503bd319717ef5be83d5
j. schere <48649270+scherepi@users.noreply.github.com>
2026-07-02 15:45:09 -0500

    hit the quan liquidate style (#1)

--- rules.rb
+++ rules.rb
@@ -22,6 +22,7 @@ config do
     redirect "jobs.hackclub.com", to: "https://hackclub.com/jobs"
     redirect "pizza.hackclub.com", to: "https://hackclub.com/pizza", code: 302
     redirect "sinerider.hackclub.com", to: "https://sinerider.com", code: 302
+    redirect "liquidate.hackclub.com", to: "https://itch.io/jam/liquidate", code: 302
 
     # hackclub.id
     redirect "hackclub.id", to: "https://auth.hackclub.com"
